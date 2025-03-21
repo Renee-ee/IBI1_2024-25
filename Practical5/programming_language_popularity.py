872715c09ee50b633652e3ae8726979fe3e4de25
Renee-ee <yirutang999@gmail.com>
2025-03-21 10:20:14 +0800

    change program1

--- Practical5/programming_language_popularity.py
+++ Practical5/programming_language_popularity.py
@@ -1,11 +1,20 @@
+#firstly put all the data into a dictionary lan
 lan = {'JavaScipt': 62.3, 'HTML': 52.9, 'Python': 51, 'SQL': 51, "TypeScript": 38.5 }
 print (lan)
 
+#use the matplot to draw a plot
 import matplotlib.pyplot as plt
 import numpy as np
-
+#setting the bar chart
 plt.bar(lan.keys(), lan.values(), color='cyan')
 plt.xlabel('Language')
 plt.ylabel('User(percentage)')
 plt.title('Programming language popularity')
 plt.show()
+
+#out put the input language percentage
+requested_language = "Python"  # <- this can be changed
+if requested_language in lan:
+    print(f"\nThe percentage of developers who use {requested_language} is {lan[requested_language]}%.")
+else:
+    print(f"\n{requested_language} is not in the data.")
