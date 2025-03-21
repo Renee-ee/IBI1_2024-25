f20c1d1c3b14e04bb6a4dbfec4a59b625d21a343
Renee-ee <yirutang999@gmail.com>
2025-03-21 22:23:23 +0800

    new

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,4 +1,6 @@
+#let the sum s firstly be 0
+#then for i in range(1,11), for loop
 s=0
-for i in range(1,10):
-    s=s+i
-    print (s)
+for i in range(1,11):
+    s+=i
+    print ("the number of dots for triangle",i,"is",s) #print the sum s of each i
