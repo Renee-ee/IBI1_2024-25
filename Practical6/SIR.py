518e68ca54c26be030c3235f5adc26a5eaa301f0
Renee-ee <yirutang999@gmail.com>
2025-03-29 02:22:37 +0800

    SIR_2

--- Practical6/SIR.py
+++ Practical6/SIR.py
@@ -26,22 +26,24 @@ for n in range (1,1001):
     I_minus = 0
     for i in range (0,I):
         b=np.random.choice (range(2),1,p=[1-gamma, gamma])
-        if a == 1:
+        if b == 1:
             I_minus -= 1
             R += 1
     I = I + I_add - I_minus
-    S = P-S-I
+    S = P-R-I
     arr_S.append(S) # Add element
     arr_I.append(I) # Add element
     arr_R.append(R) # Add element
 
 #plot the result
 plt.plot(time, arr_S, label="Susceptible", color="blue") #Susceptible in blue
-plt.plot(time, arr_I, label="infected", color="red") #Infected in red
-plt.plot(time, arr_S, label="Infected", color="green") #Recovered in green
+plt.plot(time, arr_I, label="Infected", color="red") #Infected in red
+plt.plot(time, arr_S, label="Recovered", color="green") #Recovered in green
 plt.xlabel("time")
 plt.ylabel("number of people")
 plt.title("SIR model")
-plt.legend()
-
+plt.legend() #add legend
 plt.show()
+#save the plot as a file
+plt.figure(figsize =(6,4),dpi=150) # set your plots as a file with this dimensions and resolution
+plt.saving("<C:\Users\Administrator\OneDrive - International Campus, Zhejiang University\桌面\IBI1_2024-25\Practical6\SIR>", type="png")
