710860dcec71faab305b6f17bf40f80399679170
Renee-ee <yirutang999@gmail.com>
2025-04-03 02:32:08 +0800

    add comment

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -1,13 +1,17 @@
-a=15
-b=75
-c=a+b
-d=90
-e=5
-f=d+e
-# f is longer
-# c is quicker
+#4.1 Some simple math 
+a=15 #The walk to the bus stop is 15 mins
+b=1*60+15 #The bus journey takes 1 hr and 15 mins
+c=a+b #the total length of time
 
+d=1*60+30 #The drive takes 1 hr and 30 mins
+e=5  #the walk from the car park takes 5 mins
+f=d+e #the total length of time for the car-based commute
+# compare c to f: f is longer
+# c(walk to the bus stop and take the bus directly to their office) is quicker
+
+
+#4.2 Booleans
 X = True
 Y = False
-W = X and Y
+W = X and Y #a new variable called W which encodes the Boolean variable ‘both X and Y’
 # W is False
