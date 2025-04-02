5e154fb3382f69ed55fe07602ec717a8f28a8e25
Renee-ee <yirutang999@gmail.com>
2025-04-03 02:37:25 +0800

    add more pseudocode

--- Practical4/triangle.py
+++ Practical4/triangle.py
@@ -1,5 +1,6 @@
-#let the sum s firstly be 0
-#then for i in range(1,11), for loop
+#we can observe that the the number of dots for triangle n is (1+2+3+...+n)
+#so let the sum s firstly be 0
+#then for i in range(1,11), for loop to get the number of dots in triangle in 10 triangles
 s=0
 for i in range(1,11):
     s+=i
