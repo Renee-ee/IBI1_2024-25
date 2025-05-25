e2f96f6b1be8b74bc2fc31368b3f57f1b6c7833f
Renee-ee <yirutang999@gmail.com>
2025-05-26 00:31:10 +0800

    restriction enzyme

---
+++ Practical8/restriction_enzyme_cut_sites.py
@@ -0,0 +1,24 @@
+def cut(DNA, recog):
+    #check that both sequences contain only canonical (‘ACGT’) nucleotides
+    nucl=['A','C','G','T'] #canonical (‘ACGT’) nucleotides
+    for n in DNA:
+        if n not in nucl:
+            return ('Error: Invalid nucleotide in DNA sequence')
+    for n in recog:
+        if n not in nucl:
+            return ('Error: Invalid nucleotide in recognition sequence')
+    # find the position
+    l_recog=len(recog)
+    for i in range(0, len(DNA)-l_recog+1):
+        win= DNA[i: i+l_recog]
+        if win== recog:
+            output='DNA sequence position where the restriction enzyme cuts: '+ str(i+1)
+            return(output)
+    return('Not found')
+
+
+#get input
+DNA= 'ATTTCCTGCGTA'
+rec= 'ATT'
+
+print(cut(DNA,rec))
