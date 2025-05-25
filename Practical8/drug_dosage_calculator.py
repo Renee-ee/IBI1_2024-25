c5b4889d0bc490887ae5432d4be20a2081b8a161
Renee-ee <yirutang999@gmail.com>
2025-05-25 23:33:24 +0800

    add check

--- Practical8/drug_dosage_calculator.py
+++ Practical8/drug_dosage_calculator.py
@@ -1,13 +1,14 @@
 #For children up to the age of 18, the recommended dose of the painkiller paracetamol is 15 mg/kg.
 
 def drug (w,s):
-    if w<10 or w>100 or (s!= 120 and s!= 250): #a number of checks
-        return "error"
-    else: # if all the check is right, calculate the volume of paracetamol required
-        vol = w*s
-        return vol
+    if w<10 or w>100: #a number of checks
+        return "weight error"
+    if s!= 120 and s!= 250: #a number of checks
+        return "strength of paracetamol error"
+    vol = w*s
+    return vol
 
-w = 50 #get the weight
+w = 45 #get the weight
 s = 120 #get the strength of paracetamol:120 for 120mg/5ml or 250 for 250mg/5ml
 
 print(drug (w,s))
