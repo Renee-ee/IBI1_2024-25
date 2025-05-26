07533c852adf9d493685a182729d9a65bf044ae4
Renee-ee <yirutang999@gmail.com>
2025-05-26 15:35:39 +0800

    add explaination

--- Practical8/restriction_enzyme_cut_sites.py
+++ Practical8/restriction_enzyme_cut_sites.py
@@ -12,8 +12,7 @@ def cut(DNA, recog):
     for i in range(0, len(DNA)-l_recog+1):
         win= DNA[i: i+l_recog]
         if win== recog:
-            output='DNA sequence position where the restriction enzyme cuts: '+ str(i+1)
-            return(output)
+            output='DNA sequence position where the restriction enzyme cuts: '+ str(i+1) # the first nucleotide in the DNA sequence is considered to be at position 1, i is from 0, so the position is (i+1)
     return('Not found')
 
 
