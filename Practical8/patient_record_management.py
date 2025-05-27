3d97d538720291e0bd7b085dd4048e3e013428c6
Renee-ee <yirutang999@gmail.com>
2025-05-28 04:02:31 +0800

    add comment

--- Practical8/patient_record_management.py
+++ Practical8/patient_record_management.py
@@ -1,10 +1,13 @@
+# Define a class to manage patient records
 class patient:
     def __init__ (self, name, age, date, history):
+        # Assign input values to object attributes
         self.name = name
         self.age = age
         self.date = date
         self.history = history
     def pri(self):
+        # A fuction displays formatted patient information
         print ("Patient name:", self.name,'|', "Age:", self.age,'|', "Date of latest admission:", self.date,'|', "Medical history:", self.history)
 
 #name = Resoo
