525f4b3fd15ff5e05534ce80db342710e7813289
Renee-ee <yirutang999@gmail.com>
2025-05-28 02:51:52 +0800

    add comment

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -1,11 +1,12 @@
 seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
 
+#import necessary library
 import re
-match = re.search(r'GT.*AG',seq)
+match = re.search(r'GT.*AG',seq) #search the largest(greedy) sequence start with GT and end with AG
 
 if match:
-    matched_seq = match.group()
-    max_len = len(matched_seq)
+    matched_seq = match.group() #get that sequence
+    max_len = len(matched_seq) # get length of that sequence
     print("the largest length of intron is",max_len)
-else:
+else: # if haven't found that
     print("No match found.")
