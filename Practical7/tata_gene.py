558671eeaef6e683ab9885c27a0987bb070c54e0
Renee-ee <yirutang999@gmail.com>
2025-05-28 02:20:46 +0800

    output sequence into oneline

--- Practical7/tata_gene.py
+++ Practical7/tata_gene.py
@@ -33,7 +33,6 @@ for one_gene in GENE_list:
     #Search for TATA_box
     if tata_pattern.search(sequence):
         output.write('>'+ gene_name +'\n') # the header line containing only the gene name
-        for i in range(0, len(sequence), 150):
-            output.write(sequence[i:i+150] + '\n') # Note that the sequences for each gene run onto several lines.	
+        output.write(sequence + '\n')
 
 output.close()
